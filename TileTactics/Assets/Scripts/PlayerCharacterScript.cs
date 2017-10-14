6a0fedb810f99464f2d15a76203ad943a7c9b512
Michael Lucaci <mlucaci3@gatech.edu>
2017-10-14 15:58:39 -0400

    Added attacking recognition

--- TileTactics/Assets/Scripts/PlayerCharacterScript.cs
+++ TileTactics/Assets/Scripts/PlayerCharacterScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerCharacterScript : MonoBehaviour {
 
     private PlayerCharacter character;
+    private bool attacking = false;
 
 	// Use this for initialization
 	void Start () {
@@ -20,5 +21,31 @@ public class PlayerCharacterScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Debug.Log("Character name: " + character.Name);
+        
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            attacking = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Backspace)) {
+            attacking = false;
+            Debug.Log("Not attacking");
+        }
+
+        if (attacking) {
+            Debug.Log("In attacking condition");
+            if (Input.GetKeyDown(KeyCode.W)) {
+                Debug.Log("Attacked North");
+                attacking = false;
+            } else if (Input.GetKeyDown(KeyCode.A)) {
+                Debug.Log("Attacked West");
+                attacking = false;
+            } else if (Input.GetKeyDown(KeyCode.S)) {
+                Debug.Log("Attacked South");
+                attacking = false;
+            } else if (Input.GetKeyDown(KeyCode.D)) {
+                Debug.Log("Attacked East");
+                attacking = false;
+            }
+        }
 	}
 }
