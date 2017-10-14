63788903913932817b26a1ee586d0cc2ae90b6f7
Michael Lucaci <mlucaci3@gatech.edu>
2017-10-14 17:56:35 -0400

    Changed materials depending on player's action choice

--- TileTactics/Assets/Scripts/PlayerCharacterScript.cs
+++ TileTactics/Assets/Scripts/PlayerCharacterScript.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class PlayerCharacterScript : MonoBehaviour {
 
     private PlayerCharacter character;
+    public Character.Weapon characterWeapon;
 
 	// Use this for initialization
 	void Start () {
-        if (transform.tag.Equals("SwordPlayer")) {
+        character = new PlayerCharacter(3, characterWeapon, gameObject.name);
+        /*if (transform.tag.Equals("SwordPlayer")) {
             character = new PlayerCharacter(3, Character.Weapon.SWORD, gameObject.name);
         } else if (transform.tag.Equals("LancePlayer")) {
             character = new PlayerCharacter(3, Character.Weapon.LANCE, gameObject.name);
         } else if (transform.tag.Equals("BowPlayer")) {
             character = new PlayerCharacter(3, Character.Weapon.BOW, gameObject.name);
-        }
+        }*/
 	}
 	
 	// Update is called once per frame
